4f839212ae642ab78e538a0ff8ff10ccedddf389
alberto <albertofgaldo@gmail.com>
2018-09-28 14:57:05 +0200

    Viernes 28/09 Get Ok Carga Imagen OK Carga en FireBase  OK Recoger url FireBase No

--- app/src/main/java/com/picolab/view/paintImage.java
+++ app/src/main/java/com/picolab/view/paintImage.java
@@ -1,27 +1,53 @@
 package com.picolab.view;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.picolab.R;
 import com.rm.freedrawview.FreeDrawView;
 import com.rm.freedrawview.PathDrawnListener;
 import com.rm.freedrawview.PathRedoUndoCountChangeListener;
 import com.rm.freedrawview.ResizeBehaviour;
 
+
 import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+
+import static com.picolab.view.showImage.canvasImage;
 
 public class paintImage extends AppCompatActivity {
 
     FreeDrawView mSignatureView;
     Button btn_save;
     TextView whiteButton, redButton, blueButton, yellowButton, greenButton, blackButton, orangeButton, pinkButton, purpleButton, skyButton, clearButton, grayButton;
+    //FirebaseStorage storage = FirebaseStorage.getInstance();
+
+
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +66,8 @@ public class paintImage extends AppCompatActivity {
         skyButton = (TextView)findViewById(R.id.colorSkyButton);
         grayButton = (TextView)findViewById(R.id.colorGrayButton);
         clearButton = (TextView)findViewById(R.id.clearButton);
-
-
         btn_save = (Button) findViewById(R.id.btn_save);
         mSignatureView = (FreeDrawView) findViewById(R.id.parentView);
-        // Setup the View
 
        // mSignatureView.setPaintColor(Color.BLACK); //modifica el color con el que pinta
         mSignatureView.setPaintWidthPx(12);
@@ -99,17 +122,9 @@ public class paintImage extends AppCompatActivity {
                     @Override
                     public void onDrawCreated(Bitmap draw) {
                         //TODO:Aqui ya puedes coger el Bitmap y hacer lo que quieras
-                        Toast.makeText(paintImage.this, "tengo el bitmap, envíalo", Toast.LENGTH_SHORT).show();
-
-                      /*  FriendlyMessage friendlyMessage = new
-                                FriendlyMessage(mMessageEditText.getText().toString(),
-                                mUsername,
-                                mPhotoUrl,
-                                null *//* no image *//*);
-                        mFirebaseDatabaseReference.child(MESSAGES_CHILD)
-                                .push().setValue(friendlyMessage);
-                        mMessageEditText.setText("");
-                        btn_save.setEnabled(false);*/
+                        Toast.makeText(paintImage.this, "Tu dibujo se ha enviado", Toast.LENGTH_SHORT).show();
+                        uploadImage(draw);
+                        btn_save.setEnabled(false);
                     }
                     @Override
                     public void onDrawCreationError() {
@@ -190,6 +205,37 @@ public class paintImage extends AppCompatActivity {
             public void onClick(View view) {
                 mSignatureView.clearDraw();
                 mSignatureView.setPaintColor(Color.BLACK);
+            }
+        });
+    }
+
+    private void uploadImage(Bitmap draw) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        draw.compress(Bitmap.CompressFormat.JPEG,100, baos);
+        byte[] data = baos.toByteArray();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://picolab-c4b9b.appspot.com");
+
+        // Create a reference to "mountains.jpg"
+        StorageReference mountainsRef = storageRef.child(canvasImage.getId() + ".jpg");
+
+        // Create a reference to 'images/mountains.jpg'
+        //StorageReference mountainImagesRef = storageRef.child(canvasImage.getId() + ".jpg");
+
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+               // Uri downloadUrl = taskSnapshot.getDownloadUrl();
             }
         });
     }
